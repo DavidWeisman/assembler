471c104db165824fe65535544df6c16631e2ff65
David <davidweisman22@gmail.com>
2024-03-02 21:09:47 +0200

    update files

---
+++ globals.h
@@ -0,0 +1,38 @@
+
+/* Commands opcode */
+typedef enum opcodes {
+	MOV_OP = 0,
+	CMP_OP = 1,
+	ADD_OP = 2,
+	SUB_OP = 2,
+	LEA_OP = 4,
+	CLR_OP = 5,
+	NOT_OP = 5,
+	INC_OP = 5,
+	DEC_OP = 5,
+	JMP_OP = 9,
+	BNE_OP = 9,
+	JSR_OP = 9,
+	RED_OP = 12,
+	PRN_OP = 13,
+	RTS_OP = 14,
+	STOP_OP = 15,
+
+	/** Failed/Error */
+	NONE_OP = -1
+} opcode;
+
+
+
+/** Registers - r0->r1 + not found */
+typedef enum registers {
+	R0 = 0,
+	R1,
+	R2,
+	R3,
+	R4,
+	R5,
+	R6,
+	R7,
+	NONE_REG = -1
+} reg;
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "globals.h"
 
 
 void handel_singel_file(char *file_name);
@@ -16,7 +17,7 @@ int main(int argc, char *argv[])
         handel_singel_file(argv[number_of_file]);
 
     }
-    printf("dd");
+    printf("%d", NOT_OP);
     return 0;
 }
 
