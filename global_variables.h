3b8e0822823ee4441c318bc3e88e80865760a5c6
David <davidweisman22@gmail.com>
2024-04-03 16:54:24 +0300

    new comments for globals

--- global_variables.h
+++ global_variables.h
@@ -1,121 +1,103 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-/** Maximum length of a single line  */
+/* Maximum length of a line in the source code */
 #define MAX_LINE_LENGTH 80
 
-/** Initial IC value */
+/* Initial value for the instruction counter */
 #define IC_INIT_VALUE 100
 
-/** Maximum size of code image and data image */
+/* Length of the code and data arrays in the image file */
 #define CODE_ARR_IMG_LENGTH 1200
 
-
-/** A struct that represet the informasion of a line*/
+/* Structure to hold information about a line in the source code */
 typedef struct line_info {
-	/* Line number in file */
-	long line_number;
-	/* File name */
-	char *file_name;
-	/* Line content */
-	char *content;
+    long line_number;	/* Line number */
+    char *file_name;	/* File name */
+    char *content;		/* Content of the line */
 } line_info;
 
-/** Boolean definition*/
+
+/* Boolean values */
 typedef enum booleans {
-	FALSE = 0, TRUE = 1
+    FALSE = 0,	/* False boolean value */
+	TRUE = 1	/* True boolean value */
 } bool;
 
-/** Commands opcode */
+/* Operation codes */
 typedef enum opcodes {
-	MOV_OP = 0,
-	CMP_OP = 1,
-	ADD_OP = 2,
-	SUB_OP = 3,
-	NOT_OP = 4,
-	CLR_OP = 5,
-	LEA_OP = 6,
-	INC_OP = 7,
-	DEC_OP = 8,
-	JMP_OP = 9,
-	BNE_OP = 10,
-	RED_OP = 11,
-	PRN_OP = 12,
-	JSR_OP = 13,
-	RTS_OP = 14,
-	HLT_OP = 15,
-	NONE_OP = -1 /** In case of error*/
+    MOV_OP = 0,     /* Move operation */
+    CMP_OP = 1,     /* Compare operation */
+    ADD_OP = 2,     /* Add operation */
+    SUB_OP = 3,     /* Subtract operation */
+    NOT_OP = 4,     /* Logical NOT operation */
+    CLR_OP = 5,     /* Clear operation */
+    LEA_OP = 6,     /* Load Effective Address operation */
+    INC_OP = 7,     /* Increment operation */
+    DEC_OP = 8,     /* Decrement operation */
+    JMP_OP = 9,     /* Jump operation */
+    BNE_OP = 10,    /* Branch if Not Equal operation */
+    RED_OP = 11,    /* Read operation */
+    PRN_OP = 12,    /* Print operation */
+    JSR_OP = 13,    /* Jump to Subroutine operation */
+    RTS_OP = 14,    /* Return from Subroutine operation */
+    HLT_OP = 15,    /* Halt operation */
+    NONE_OP = -1    /* No operation */
 } opcode;
 
-/** Registers */
+/* Registers */
 typedef enum registers {
-	R0 = 0,
-	R1,
-	R2,
-	R3,
-	R4,
-	R5,
-	R6,
-	R7,
-	NONE_REG = -1 /** In case of error*/
+    R0 = 0,		/* Register 0 */
+    R1,			/* Register 0 */
+    R2,			/* Register 0 */
+    R3,			/* Register 0 */
+    R4,			/* Register 0 */
+    R5,			/* Register 0 */
+    R6,			/* Register 0 */
+    R7,			/* Register 0 */
+    NONE_REG = -1 	/* No register */
 } reg;
 
-/** Instructions types */
+/* Types of instructions */
 typedef enum instruction {
-	/** .data instruction */
-	DATA_INST,
-	/** .extern instruction */
-	EXTERN_INST,
-	/** .entry instruction */
-	ENTRY_INST,
-	/** .string instruction */
-	STRING_INST,
-	/** Not found */
-	NONE_INST,
-	/** Parsing/syntax error */
-	ERROR_INST
-} instruction;
+    DATA_INST,		/* Data instruction */
+    EXTERN_INST,	/* Extern  instruction */
+    ENTRY_INST,		/* Entry  instruction */
+    STRING_INST,	/* String  instruction */
+    NONE_INST,		/* No instruction */
+    ERROR_INST		/* Error  instruction */
+} instruction;		
 
-/** Singel code word*/
+/* Structure to represent a machine code word for instructions */
 typedef struct code_word {
-	/** First two bits: ARE*/
-	unsigned int ARE: 2;
-	/** Next two bits: destination addressing opcode*/
-	unsigned int dest_addressing: 2;
-	/** Next two bits: source addressing opcode*/
-	unsigned int src_addressing: 2;
-	/** The last four bits: opcode*/
-	unsigned int opcode: 4;
-
+    unsigned int ARE: 2;				/* Addressing Reallocation Encoding */
+    unsigned int dest_addressing: 2;	/* Destination addressing mode */
+    unsigned int src_addressing: 2;		/* Source addressing mode */
+    unsigned int opcode: 4;				/* Operation code */
 } code_word;
 
-/** Singel data word*/
+/* Structure to represent a machine code word for data */
 typedef struct data_word {
-	unsigned int ARE: 2;
-	unsigned long data;
+    unsigned int ARE: 2;	/* Addressing Reallocation Encoding */
+    unsigned long data;		/* Data value */
 } data_word;
 
-
-/* General machine code word*/
+/* Structure to represent a machine code word */
 typedef struct machine_word {
-	short length;
-	union word {
-		data_word *data;
-		code_word *code;
-	} word;
+    short length;			/* Length of the word */
+    union word {
+        data_word *data;	/* Data word */
+        code_word *code;	/* Code word */
+    } word;
 } machine_word;
 
-
+/* Types of addressing modes */
 typedef enum addressing_types {
-	/* Immediate addressing */
-	IMMEDIATE_ADDR = 0,
-	/** Direct addressing */
-	DIRECT_ADDR = 1,
-	/** Index fixed addressing */
-	INDEX_FIXED_ADDR = 2,
-	/** Register addressing */
-	REGISTER_ADDR = 3,
-	NONE_ADDR = -1 /** In case of erroe */
-}addressing_type;
+    IMMEDIATE_ADDR = 0,		/* Immediate addressing */
+    DIRECT_ADDR = 1,		/* Direct  addressing */
+    INDEX_FIXED_ADDR = 2,	/* Indexed fixed addressing */
+    REGISTER_ADDR = 3,		/* Register  addressing */
+    NONE_ADDR = -1			/* No addressing mode */
+} addressing_type;
 
-#endif 
+#endif
