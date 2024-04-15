c58d16b638886fdc0505a43b8da106029a51ee61
David <davidweisman22@gmail.com>
2024-04-15 20:32:55 +0300

    update code

--- file_writeing.c
+++ file_writeing.c
@@ -23,40 +23,6 @@
  */
 #define KEEP_ONLY_21_LSB(value) ((value) & 0x1FFFFF)
 
-void print_binary(long value) {
-    /* Mask for checking each bit */
-	int i;
-    /* Loop through each bit and print it */
-    for (i = 13; i >= 0; i--) {
-        printf("%ld", (value >> i) & 1); /* Shift and mask the bit */
-    }
-	printf("\n");
-}
-
-void print_base4(FILE *file_descriptor, int index, long value) {
-    /* Loop through each bit and print it */
-	int i;
-    fprintf(file_descriptor, "\n%.7d\t", index);
-    for (i = 12; i >= 0; i -= 2) {
-        int symbol = (value >> i) & 3; 
-        char base4_symbol;
-        switch (symbol) {
-            case 0:
-                base4_symbol = '*';
-                break;
-            case 1:
-                base4_symbol = '#';
-                break;
-            case 2:
-                base4_symbol = '%';
-                break;
-            case 3:
-                base4_symbol = '!';
-                break;
-        }
-        fprintf(file_descriptor, "%c", base4_symbol);
-    }
-}
 
 /**
  * @brief Writes machine code and data to a .ob output file.
@@ -110,46 +76,60 @@ int write_output_files(machine_word **code_img, long *data_img, long icf, long d
     return result; /* Return result of file writing operations */
 }
 
-/* Writes machine code and data to a .ob output file */
+
+/* Writes the output of the assembly process to a file in the Object (".ob") format. */
 static bool write_ob(machine_word **code_img, long *data_img, long icf, long dcf, char *filename) {
-	int index; /* Loop iterator */
+    FILE *file; /* File descriptor for the output file */
+    int i, j; /* Loop iterators */
+    int symbol; /* Symbol to represent binary values */
     long value; /* Value to be written to the file */
-    FILE *file_descriptor; /* File descriptor for the output file */
-    char *output_file; /* Filename with .ob extension */
-
-    output_file = add_extension(filename, ".ob"); /* Get output file name */
-    file_descriptor = fopen(output_file, "w"); /* Open the output file for writing */
-    free(output_file); /* Free memory allocated for the output filename */
+    char *output_file = add_extension(filename, ".ob"); /* File name with ".ob" extension */
     
-    /* Check if file is successfully opened */
-    if (file_descriptor == NULL) {
+    /* Attempt to open the output file */
+    if (!(file = fopen(output_file, "w"))) {
         printf("Can't create or rewrite to file %s.", output_file);
-        return FALSE;
+        free(output_file);
+        return FALSE; /* Failed to open file, return FALSE */
     }
-
-    /* Write headers containing the final instruction and data counters */
-    fprintf(file_descriptor, "%ld %ld", icf - IC_INIT_VALUE, dcf);
-
-    /* Write machine code lines to the file */
-    for (index = 0; index < icf - IC_INIT_VALUE; index++) {
-        if (code_img[index]->length > 0) {
-            value = (code_img[index]->word.code->opcode << 6) | (code_img[index]->word.code->src_addressing << 4) |
-                  (code_img[index]->word.code->dest_addressing << 2) | (code_img[index]->word.code->ARE);
-        } else {
-            value = (KEEP_ONLY_21_LSB(code_img[index]->word.data->data) << 2) | (code_img[index]->word.data->ARE);
+    free(output_file); /* Free memory allocated for the output file name */
+    
+    /* Write the header of the object file */
+    fprintf(file, "%ld %ld", icf - IC_INIT_VALUE, dcf);
+    
+    /* Write the instructions to the object file */
+    for (i = 0; i < icf - IC_INIT_VALUE; ++i) {
+        /* Construct the value to be written based on the machine word */
+        value = code_img[i]->length > 0 ?
+                (code_img[i]->word.code->opcode << 6) | (code_img[i]->word.code->src_addressing << 4) |
+                (code_img[i]->word.code->dest_addressing << 2) | (code_img[i]->word.code->ARE) :
+                (KEEP_ONLY_21_LSB(code_img[i]->word.data->data) << 2) | (code_img[i]->word.data->ARE);
+                
+        /* Write the address followed by the binary representation of the value */
+        fprintf(file, "\n%.7d\t", i + 100);
+        for (j = 12; j >= 0; j -= 2) {
+            symbol = (value >> j) & 3; 
+            fputc("*#%!"[symbol], file); /* Write the appropriate symbol to represent the binary value */
         }
-        print_base4(file_descriptor, index + 100, value); /* Print binary representation to console */
+    }
+    
+    /* Write the data to the object file */
+    for (i = 0; i < dcf; ++i) {
+        /* Write the address followed by the binary representation of the data */
+        fprintf(file, "\n%.7d\t", i + icf);
+        value = KEEP_ONLY_24_LSB(data_img[i]);
+        for (j = 12; j >= 0; j -= 2) {
+            symbol = (value >> j) & 3; 
+            fputc("*#%!"[symbol], file); /* Write the appropriate symbol to represent the binary value */
+        }
     }
 
-    /* Write data lines to the file */
-    for (index = 0; index < dcf; index++) {
-        value = KEEP_ONLY_24_LSB(data_img[index]);
-        print_base4(file_descriptor, index + icf, value); /* Print binary representation to console */
-    }
-
-    fclose(file_descriptor); /* Close the output file */
-    return TRUE; /* Return true indicating successful operation */
+    fclose(file); /* Close the file */
+    return TRUE; /* Successfully wrote the object file, return TRUE */
 }
+
+
+
+
 
 /* Writes a table to a file with the specified filename and extension */
 static bool write_table_to_file(table tab, char *filename, char *file_extension) {
